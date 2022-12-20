460717e849589feeb3511d79d0ab84d69bf015e8
Carl-with-a-C <carlbclifton@gmail.com>
2022-12-20 15:03:07 +0000

    make icons appear and boundaries of map size

--- struction/src/components/SiteMap.js
+++ struction/src/components/SiteMap.js
@@ -4,20 +4,27 @@ import { MapContainer, ImageOverlay } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import testMarkerData from "../DB/testData/testMarkerData.json";
 import SiteMarkers from "./SiteMarkers";
+import L from "leaflet";
 
 const SiteMap = () => {
-  const L = window["L"];
+  // const L = window["L"];
   const bounds = [
-    [-1000, -1000],
-    [1000, 1000],
+    [0, 1000],
+    [1000, 0],
   ];
+
+  const mapSW = [0, 1000];
+  const mapNE = [1000, 0];
 
   return (
     <main className="siteMap">
       <MapContainer
+        zoomControl={false}
         className="map"
         crs={L.CRS.Simple}
         bounds={bounds}
+        maxBounds={bounds}
+        maxBoundsViscosity={0.9}
         zoom={0}
         minZoom={-3}
         maxZoom={1}

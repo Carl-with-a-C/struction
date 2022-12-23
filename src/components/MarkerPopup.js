5f18b7c9d7401b023af2e36619e4dc1902517c7d
aelorn13 <a.maslov364@gmail.com>
2022-12-23 11:33:47 +0000

    added basic Popup text

--- src/components/MarkerPopup.js
+++ src/components/MarkerPopup.js
@@ -2,11 +2,21 @@ import React from "react";
 import { Popup } from "react-leaflet";
 
 const MarkerPopup = ({ marker }) => {
-  const { shortDescription } = marker;
-
   return (
     <Popup>
-      <div className="popup-text">{shortDescription}</div>
+      <div className="popup-text">
+        <div>id: {marker.id}</div>
+        <div>Required service: {marker.service}</div>
+        <div>Photos:{marker.photos}</div>
+        <div>Photos after:{marker.photos_after}</div>
+        <div>
+          Measuremenets:{" "}
+          {marker.measurements.map((number) => {
+            return number + " ";
+          })}
+        </div>
+        <div>Completed by: {marker.completedBy}</div>
+      </div>
     </Popup>
   );
 };

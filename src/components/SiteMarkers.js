a368e376f78228c8090d27f6f8e716fd15ea547d
twissy22 <adamdtwiss@hotmail.com>
2022-12-23 11:11:09 +0000

    adding patch to button

--- src/components/SiteMarkers.js
+++ src/components/SiteMarkers.js
@@ -1,53 +1,44 @@
 import { Fragment } from "react";
 import { React, useState, useEffect, useMemo, useRef } from "react";
-import {patchMarker} from "../utils/api.js"
+import { patchMarker } from "../utils/api.js";
 import { Marker } from "react-leaflet";
 import MarkerPopup from "./MarkerPopup";
 import pinIcon from "../assets/DefaultPin.svg";
 import L from "leaflet";
 
-
 const center = {
   lat: 51.505,
   lng: -0.09,
-}
+};
 
 const SiteMarkers = ({ markersData, currFloor }) => {
   const [pins, setPins] = useState([]);
-  const [activePin, setActivePin]= useState(null)
-  const [position, setPosition] = useState(center)
-  const markerRef = useRef(null)
+  const [activePin, setActivePin] = useState(null);
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null);
   const icon = L.icon({
     iconUrl: pinIcon,
     iconSize: [40, 46],
-    iconAnchor: [20, 46],   
+    iconAnchor: [20, 46],
   });
- 
-  const eventHandlers = useMemo(
 
+  const eventHandlers = useMemo(
     () => ({
       dragend() {
-        const marker = markerRef.current
+        const marker = markerRef.current;
         if (marker != null) {
-          
-          const cords = marker.getLatLng()
-          console.log(cords.lng)
-          let patch = markerRef.current.options.value
-          
-          console.log(Math.floor(cords.lat).toString())
-          patch.locationOnDrawing[0] = Math.floor(cords.lat).toString()
-          patch.locationOnDrawing[1] = Math.floor(cords.lng).toString()
-          let markerid = markerRef.current.options.value.id
-
-          console.log(patch)
-          patchMarker(markerid, patch)
-          
+          const cords = marker.getLatLng();
+          let patch1 = markerRef.current.options.value;
+          patch1.locationOnDrawing[0] = Math.floor(cords.lat).toString();
+          patch1.locationOnDrawing[1] = Math.floor(cords.lng).toString();
+          let markerid = markerRef.current.options.value.id;
+          const patch = { [markerid]: patch1 };
+          patchMarker(markerid, patch);
         }
       },
     }),
-    [], 
-  
-  )
+    []
+  );
   useEffect(() => {
     const returnedPins = markersData.map((marker) => {
       if (marker.location === currFloor)
@@ -59,12 +50,8 @@ const SiteMarkers = ({ markersData, currFloor }) => {
             ref={markerRef}
             value={marker}
             eventHandlers={eventHandlers}
-           
             draggable={true}
             autoPan={false}
-            
-            
-          
           >
             <MarkerPopup marker={marker} />
           </Marker>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ProSidebarProvider } from "react-pro-sidebar";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  
   <ProSidebarProvider>
+   
     <App />
+ 
   </ProSidebarProvider>
 );
 
